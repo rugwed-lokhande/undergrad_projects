627d78e9e1ecddb29672c066f88510b1e834f997
rugwed-lokhande <101607593+rugwed-lokhande@users.noreply.github.com>
2023-04-03 14:05:46 -0400

    Add files via upload

---
+++ read_File.py
@@ -0,0 +1,25 @@
+#enter your file name in place of input
+
+f=open("input",'r')
+data=f.read()
+data=data.split("\n")
+length=int(data[0])
+data_rev=[]
+for i in range(length):
+    data_rev.append(float(data[i+1]))
+
+Numlist=data_rev
+Number=len(data_rev)
+for i in range (Number) :
+    for j in range(i+1, Number):
+        if(Numlist[i] > Numlist[j]):
+           temp = Numlist[i]
+           Numlist[i] = Numlist[j]
+           Numlist[j] = temp
+print("Element after sorting list in ascending order is:", Numlist)
+
+#enter output file name in place of output          
+f=open("output.txt",'w')
+for k in Numlist:
+    f.write(str(k)+"\n")
+f.close()
